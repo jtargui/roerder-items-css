cad8e0d86d322377e901258ad2fe43bf7020e6d1
Jordi <jtargui@gmail.com>
2020-05-04 21:03:34 +0200

    test barcode y test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {DragulaModule} from "ng2-dragula";
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -14,6 +23,15 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    DragulaModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +42,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {TranslateService} from "@ngx-translate/core";
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+import 'rxjs/add/observable/interval';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage implements OnInit {
 
+  items = [];
+  sub: any;
+  constructor(public navCtrl: NavController, translate: TranslateService, public barcodeScanner: BarcodeScanner) {
+    translate.setDefaultLang('es');
+    for (let x = 0; x < 50; x++) {
+      this.items.push({id:x, name: x});
+    }
+  }
+
+  ngOnInit(): void {
+    this.startSubroutine();
+  }
+
+  startSubroutine() {
+    if (this.sub)
+      return;
+
+    this.sub = Observable.interval(1000)
+      .subscribe((val) => {
+        console.log('called', this.items);
+        if (this.items.length>0) {
+          this.items.pop();
+        }
+      });
+
+  }
+
+  stopSubRoutine() {
+    this.sub.unsubscribe();
+    this.sub = undefined;
+  }
+
+  reorderItems(indexes) {
+    let element = this.items[indexes.from];
+    this.items.splice(indexes.from, 1);
+    this.items.splice(indexes.to, 0, element);
+  }
+
+  testAlter() {
+    alert("test");
+  }
+
+  readBarCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
 
 }
